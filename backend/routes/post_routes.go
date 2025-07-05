c5c82bcf41d919bc54523438c86fcb2cf82963a8
sasanzare <sasanzare75@gmail.com>
2025-07-05 20:24:53 +0330

    feat(routes): implement basic post routes

--- backend/routes/post_routes.go
+++ backend/routes/post_routes.go
@@ -1,1 +1,17 @@
 package routes
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/sasanzare/go-cms/controllers"
+	"github.com/sasanzare/go-cms/middleware"
+)
+
+func SetupPostRoutes(r *gin.Engine) {
+	posts := r.Group("/api/posts")
+	{
+		posts.GET("", controllers.ListPosts)
+		posts.GET("/:id", controllers.GetPost)
+		posts.POST("", middleware.AuthMiddleware(), controllers.CreatePost)
+		posts.PUT("/:id", middleware.AuthMiddleware(), controllers.UpdatePost)
+	}
+}
