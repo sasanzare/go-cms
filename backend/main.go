dc2de56ee92988a78e5ad591daf21c1582e04118
sasanzare <sasanzare75@gmail.com>
2025-07-05 20:28:11 +0330

    feat(main): initialize post routes in main entrypoint

--- backend/main.go
+++ backend/main.go
@@ -1,14 +1,12 @@
 package main
 
 import (
-  "github.com/gin-gonic/gin"
-//   "gorm.io/gorm"
+	"github.com/sasanzare/go-cms/routes"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-  r := gin.Default()
-  r.GET("/ping", func(c *gin.Context) {
-    c.JSON(200, gin.H{"message": "Hi"})
-  })
-  r.Run() // localhost:8080
+	r := gin.Default()
+	routes.SetupPostRoutes(r)
+	r.Run(":8000")
 }
