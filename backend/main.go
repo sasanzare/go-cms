0de433046e60d738b1dbc4a4a5a549fe9a9a4c46
sasanzare <sasanzare75@gmail.com>
2025-07-07 12:31:18 +0330

    refactor: update main.go

--- backend/main.go
+++ backend/main.go
@@ -1,12 +1,38 @@
 package main
 
 import (
-	"github.com/sasanzare/go-cms/routes"
+	"log"
+
 	"github.com/gin-gonic/gin"
+	"github.com/sasanzare/go-cms/config"
+	"github.com/sasanzare/go-cms/migrations"
+	"github.com/sasanzare/go-cms/routes"
 )
 
 func main() {
+	// Load configuration
+	dbConfig := config.LoadDBConfig()
+
+	// Connect to database
+	db, err := config.ConnectDB(dbConfig)
+	if err != nil {
+		log.Fatalf("Failed to connect to database: %v", err)
+	}
+
+	// Run auto migrations
+	if err := migrations.InitAutoMigrations(db); err != nil {
+		log.Fatalf("Failed to run auto migrations: %v", err)
+	}
+
+	// Create Gin router
 	r := gin.Default()
-	routes.SetupPostRoutes(r)
-	r.Run(":8000")
+
+	// Setup main routes
+	routes.SetupRouter(r, db)
+
+	// Start server
+	log.Println("Starting server on :8000")
+	if err := r.Run(":8000"); err != nil {
+		log.Fatalf("Failed to start server: %v", err)
+	}
 }
